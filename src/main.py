8f5809f012298f2dc167e047fb7e4bce45e5a19d
Anhad Jai Singh <ffledgling@gmail.com>
2018-10-11 12:22:37 +0530

    Start cryptographically verifying responses
    * Verify pub-key in certificate is correctly signed
    * Verify the timestamp is signed correctly using the verified pub-key
    * Verify the nonce is in the merkel tree (root-only case at the moment)

--- src/main.py
+++ src/main.py
@@ -4,11 +4,23 @@ import socket
 import struct
 import pprint
 import datetime
+import hashlib
 
 # https://pypi.org/project/ed25519/
 # https://github.com/warner/python-ed25519
 # https://blog.mozilla.org/warner/2011/11/21/introducing-python-ed25519/
 import ed25519
+
+# FIXME: Move the exceptions out into their own file so that they're exposed to
+# the user as mypkg.exceptions.* and not mypkg.*
+class RoughTimeError(Exception):
+    pass
+
+class MalformedSectionError(RoughTimeError):
+    pass
+
+class NotImplementedError(RoughTimeError):
+    pass
 
 def print_protocol_packet(packet):
     print(type(packet))
@@ -26,6 +38,8 @@ def deconstruct_SREP(section):
 
     print("Time is:", midpoint, "±", radius, "µs (UTC)")
     print("Local Time:", datetime.datetime.fromtimestamp(midpoint/10**6), "±", datetime.timedelta(microseconds=radius))
+
+    return midpoint, radius
 
 def verify_cert(packet):
 
@@ -66,18 +80,51 @@ def deconstruct_resp(packet):
     #print(data)
     pprint.pprint(data)
 
-    if b'SREP' in data:
-        time_struct = deconstruct_resp(data[b'SREP'])
-        deconstruct_SREP(time_struct)
-
-    if b'CERT' in data:
-        cert = deconstruct_resp(data[b'CERT'])
-        cert_dele = deconstruct_resp(cert[b'DELE'])
-        #cert[b'DELE'] = cert_dele
-        verify_cert(cert)
-
     return data
 
+def verify(data):
+
+    if b'SREP' not in data:
+        raise MalformedSectionError('Missing SREP tag')
+
+    time_struct = deconstruct_resp(data[b'SREP'])
+    midpoint, radius = deconstruct_SREP(time_struct)
+
+
+    if b'CERT' not in data:
+        raise MalformedSectionError('Missing CERT tag')
+
+    cert = deconstruct_resp(data[b'CERT'])
+    cert_dele = deconstruct_resp(cert[b'DELE'])
+    #cert[b'DELE'] = cert_dele
+    verify_cert(cert)
+    mint = struct.unpack('<Q', cert_dele[b'MINT'])[0]
+    maxt = struct.unpack('<Q', cert_dele[b'MAXT'])[0]
+    pubk = ed25519.VerifyingKey(cert_dele[b'PUBK'])
+
+    assert mint <= midpoint and midpoint <= maxt
+
+    if b'INDX' not in data:
+        raise MalformedSectionError('Missing INDX tag')
+
+    indx = struct.unpack('<L', data[b'INDX'])[0]
+    print("INDX: ", indx)
+    # Check nonce is part of the tree
+    if indx != 0:
+        raise NotImplementedError("Merkel Tree traversal is not yet implemented in this client")
+
+    srep = deconstruct_resp(data[b'SREP'])
+    # Verify our nonce is in the signed response
+    assert hashlib.sha512(TREE_LEAF_HASH_PREFIX + nonc).digest() == srep[b'ROOT']
+
+
+    # Verify timestamp is signed
+    try:
+        pubk.verify(sig=data[b'SIG\x00'], msg=(SIGNED_RESPONSE_CONTEXT+data[b'SREP']))
+        print('Timestamp verified')
+    except ed25519.BadSignatureError:
+        print('Timestamp sent by server could not be verified')
+        raise
 
 SERVER_FQDN='roughtime.cloudflare.com'
 SERVER_PORT=2002
@@ -89,6 +136,12 @@ SERVER_PUBKEY='gD63hSj3ScS+wuOeGrubXlq35N1c5Lby/S+T7MNTjxo='
 # https://github.com/cloudflare/roughtime/blob/master/vendor/roughtime.googlesource.com/roughtime.git/go/protocol/protocol.go#L36-L37
 CERTIFICATE_CONTEXT=b'RoughTime v1 delegation signature--\x00'
 SIGNED_RESPONSE_CONTEXT=b'RoughTime v1 response signature\x00'
+
+# These need to prefixed when computing the relevant hash, it's part of the
+# protocol specificaion. See hashLeaf and hashNode here -
+# https://roughtime.googlesource.com/roughtime/+/master/PROTOCOL.md#authenticating-replies
+TREE_LEAF_HASH_PREFIX=b'\x00'
+TREE_NODE_HASH_PREFIX=b'\x00'
 
 
 SERVER = (SERVER_FQDN, SERVER_PORT)
@@ -103,13 +156,14 @@ We're not using any offsets in the simplest case
 64 # Size of offset (from where tags finish) for PAD\xff
 NONC # tagname
 PAD\xff # tagname for pading a UDP packet
-TESTREQ\x00 # tag value
+TESTREQ\xff # tag value
 \x00 ... upto 1024 bytes of the packet
 """
 
 # TODO: add more checks for padding and string length correctly here
-byte_format_string = '<LL4s4s8s'
-simple_message = (2, 64, b'NONC', b'PAD\xff', b'TESTREQ'.ljust(64, b'\xff'))
+byte_format_string = '<LL4s4s64s'
+nonc = b'TESTREQ'.ljust(64, b'\xff')
+simple_message = (2, 64, b'NONC', b'PAD\xff', nonc)
 
 message = struct.pack(byte_format_string, *simple_message)
 
@@ -118,6 +172,9 @@ message = struct.pack(byte_format_string, *simple_message)
 #print(len(message))
 
 message = message.ljust(1024, b'\x00')
+
+print('REQUEST:')
+deconstruct_resp(message)
 
 #print(len(message))
 #print(message)
@@ -129,4 +186,6 @@ status = sock.sendto(message ,SERVER)
 resp = sock.recv(1024)
 #print(type(resp))
 #print_protocol_packet(resp)
-deconstruct_resp(resp)
+print('RESPONSE:')
+data = deconstruct_resp(resp)
+verify(data)
